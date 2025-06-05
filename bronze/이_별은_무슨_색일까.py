5fb707f91a038c380cf1de1bcb4cfa3f0f9cf8e0
맥동우 <t6u80o@naver.com>
2025-06-05 09:35:37 +0900

    0605

---
+++ bronze/이_별은_무슨_색일까.py
@@ -0,0 +1,18 @@
+x = int(input())
+c = ""
+if 620 <= x <= 780:
+  c = "Red"
+if 590 <= x < 620:
+  c = "Orange"
+if 570 <= x < 590:
+  c = "Yellow"
+if 495 <= x < 570:
+  c = "Green"
+if 450 <= x < 495:
+  c = "Blue"
+if 425 <= x < 450 :
+  c = "Indigo"
+if 380 <= x < 425:
+  c = "Violet"
+
+print(c)
