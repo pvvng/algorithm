0d17d9e701ee522d16b3f0568f516ca65f9173da
맥동우 <t6u80o@naver.com>
2025-06-10 09:59:32 +0900

    0610

---
+++ bronze/코드마스터_2023.py
@@ -0,0 +1,10 @@
+x = input()
+
+if (x == "SONGDO"):
+  print("HIGHSCHOOL")
+elif (x == "CODE"):
+  print("MASTER")
+elif (x == "2023"):
+  print("0611")
+elif (x == "ALGORITHM"):
+  print("CONTEST")
