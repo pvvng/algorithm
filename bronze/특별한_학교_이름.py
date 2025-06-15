60cb8937776ca34449f47753828e0ce4bfa42bf0
맥동우 <t6u80o@naver.com>
2025-06-16 08:58:48 +0900

    0616

---
+++ bronze/특별한_학교_이름.py
@@ -0,0 +1,9 @@
+x = input()
+if(x == "NLCS"):
+  print( "North London Collegiate School")
+if(x == "BHA") :
+  print("Branksome Hall Asia")
+if(x == "KIS") :
+  print("Korea International School")
+if(x == "SJA") :
+  print("St. Johnsbury Academy")
