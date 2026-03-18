58798732a3fc7a3f37a42253cff6861293715063
맥동우 <t6u80o@naver.com>
2026-03-19 08:46:28 +0900

    0319

---
+++ bronze/진흥원_세미나.py
@@ -0,0 +1,5 @@
+R = {"Algorithm":	"204", "DataAnalysis":	"207", "ArtificialIntelligence":	"302", "CyberSecurity":	"B101", "Network":	"303", "Startup":	"501", "TestStrategy":	"105"}
+ans = []
+for _ in range(int(input())):
+  ans.append(R[input()])
+print("\n".join(ans))
