c36d6a5699f4b2321e7ef7df9b3006b545a219cc
맥동우 <t6u80o@naver.com>
2026-04-04 23:10:39 +0900

    0404

---
+++ bronze/구멍.py
@@ -0,0 +1,8 @@
+hole = {"A": 1, "a": 1, "B": 2, "b": 1, "D": 1, "d": 1, "e": 1, "g": 1, "O": 1, "o": 1, "P": 1, "p": 1, "Q": 1, "q": 1, "R": 1, "@": 1}
+txt = input()
+ans = 0
+for c in txt:
+  find = hole.get(c)
+  if find != None:
+    ans += find
+print(ans)
